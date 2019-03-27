3fba3da548753ca11d5722b84b9ee62414d1557e
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-03-27 07:43:31 -0400

    Add color picker onto style products.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -5,25 +5,25 @@ let shades = [
     title: 'Red',
     subTitle: 'Rock-N-Stroll',
     img: '../../images/product-red.jpg',
-    price: 199
+    price: '$199'
   },
   {
     title: 'Green',
     subTitle: 'Rock-N-Stroll',
     img: '../../images/product-green.jpg',
-    price: 199
+    price: '$199'
   },
   {
     title: 'Blue',
     subTitle: 'Rock-N-Stroll',
     img: '../../images/product-blue.jpg',
-    price: 199
+    price: '$199'
   },
   {
     title: 'Black',
     subTitle: 'Rock-N-Stroll',
     img: '../../images/product-black.jpg',
-    price: 199
+    price: '$199'
   },
 ]
 
