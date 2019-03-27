9575ffa5bcc6cdb5f411b238aa34a39dc03e0985
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-03-27 08:14:25 -0400

    Add click event to each dot and colorPicker function based on the dots class.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -2,6 +2,12 @@ console.log('Working');
 
 let shades = [
   {
+    title: 'Black',
+    subTitle: 'Rock-N-Stroll',
+    img: '../../images/product-black.jpg',
+    price: '$199'
+  },
+  {
     title: 'Red',
     subTitle: 'Rock-N-Stroll',
     img: '../../images/product-red.jpg',
@@ -19,12 +25,18 @@ let shades = [
     img: '../../images/product-blue.jpg',
     price: '$199'
   },
-  {
-    title: 'Black',
-    subTitle: 'Rock-N-Stroll',
-    img: '../../images/product-black.jpg',
-    price: '$199'
-  },
 ]
 
 console.log(shades);
+
+function colorPicker(e) {
+  if (e.classList.contains('black')) {
+    console.log(shades[0]);
+  } else if (e.classList.contains('red')) {
+    console.log(shades[1]);
+  } else if (e.classList.contains('green')) {
+    console.log(shades[2]);
+  } else {
+    console.log(shades[3]);
+  }
+}
