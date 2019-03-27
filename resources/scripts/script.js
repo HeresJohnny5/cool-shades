fabaa43f4046d22eb8b9bda28de5432aafa5701d
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-03-27 13:05:11 -0400

    Refactor colorPicker function.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,20 +1,24 @@
 let shades = [
   {
+    class: 'red',
     color: 'Red',
     img: './images/product-red.jpg',
     price: '$199'
   },
   {
+    class: 'green',
     color: 'Green',
     img: './images/product-green.jpg',
     price: '$199'
   },
   {
+    class: 'blue',
     color: 'Blue',
     img: './images/product-blue.jpg',
     price: '$199'
   },
   {
+    class: 'black',
     color: 'Black',
     img: './images/product-black.jpg',
     price: '$199'
@@ -26,21 +30,11 @@ function colorPicker(e) {
   let productPriceTxt = $('.mobile-product-price');
   let productImg = $('#mobile-products .peppers-product img');
 
-  if (e.classList.contains('red')) {
-    productColorTxt.text(shades[0].color);
-    productPriceTxt.text(shades[0].price);
-    productImg.attr("src", shades[0].img);
-  } else if (e.classList.contains('green')) {
-    productColorTxt.text(shades[1].color);
-    productPriceTxt.text(shades[1].price);
-    productImg.attr("src", shades[1].img);
-  } else if (e.classList.contains('blue')) {
-    productColorTxt.text(shades[2].color);
-    productPriceTxt.text(shades[2].price);
-    productImg.attr("src", shades[2].img);
-  } else {
-    productColorTxt.text(shades[3].color);
-    productPriceTxt.text(shades[3].price);
-    productImg.attr("src", shades[3].img);
-  }
+  shades.forEach((shade, index) => {
+    if (e.classList.contains(`${shades[index].class}`)) {
+      productColorTxt.text(shades[index].color);
+      productPriceTxt.text(shades[index].price);
+      productImg.attr("src", shades[index].img);
+    }
+  });
 }
