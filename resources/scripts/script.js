985416473d79bca5b3572a92738bfc1534dbd4d0
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-03-26 20:24:03 -0400

    Create shades array.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,1 +1,30 @@
 console.log('Working');
+
+let shades = [
+  {
+    title: 'Red',
+    subTitle: 'Rock-N-Stroll',
+    img: '../../images/product-red.jpg',
+    price: 199
+  },
+  {
+    title: 'Green',
+    subTitle: 'Rock-N-Stroll',
+    img: '../../images/product-green.jpg',
+    price: 199
+  },
+  {
+    title: 'Blue',
+    subTitle: 'Rock-N-Stroll',
+    img: '../../images/product-blue.jpg',
+    price: 199
+  },
+  {
+    title: 'Black',
+    subTitle: 'Rock-N-Stroll',
+    img: '../../images/product-black.jpg',
+    price: 199
+  },
+]
+
+console.log(shades);
