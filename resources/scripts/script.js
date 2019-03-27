a7a3313247f7dce87a10ae1d26f2b4819cdec85c
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-03-27 10:21:34 -0400

    Change image upon clicking dot.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -2,27 +2,23 @@ console.log('Working');
 
 let shades = [
   {
-    title: 'Black',
-    subTitle: 'Rock-N-Stroll',
-    img: '../../images/product-black.jpg',
+    color: 'Red',
+    img: './images/product-red.jpg',
     price: '$199'
   },
   {
-    title: 'Red',
-    subTitle: 'Rock-N-Stroll',
-    img: '../../images/product-red.jpg',
+    color: 'Green',
+    img: './images/product-green.jpg',
     price: '$199'
   },
   {
-    title: 'Green',
-    subTitle: 'Rock-N-Stroll',
-    img: '../../images/product-green.jpg',
+    color: 'Blue',
+    img: './images/product-blue.jpg',
     price: '$199'
   },
   {
-    title: 'Blue',
-    subTitle: 'Rock-N-Stroll',
-    img: '../../images/product-blue.jpg',
+    color: 'Black',
+    img: './images/product-black.jpg',
     price: '$199'
   },
 ]
@@ -30,15 +26,21 @@ let shades = [
 console.log(shades);
 
 function colorPicker(e) {
-  if (e.classList.contains('black')) {
-    console.log(shades[0]);
-
-    $('.product-title').text(shades[0].title);
-  } else if (e.classList.contains('red')) {
-    console.log(shades[1]);
+  if (e.classList.contains('red')) {
+    $('.mobile-product-color').text(shades[0].color);
+    $('.mobile-product-price').text(shades[0].price);
+    $("#mobile-products .peppers-product img").attr("src", shades[0].img);
   } else if (e.classList.contains('green')) {
-    console.log(shades[2]);
+    $('.mobile-product-color').text(shades[1].color);
+    $('.mobile-product-price').text(shades[1].price);
+    $("#mobile-products .peppers-product img").attr("src", shades[1].img);
+  } else if (e.classList.contains('blue')) {
+    $('.mobile-product-color').text(shades[2].color);
+    $('.mobile-product-price').text(shades[2].price);
+    $("#mobile-products .peppers-product img").attr("src", shades[2].img);
   } else {
-    console.log(shades[3]);
+    $('.mobile-product-color').text(shades[3].color);
+    $('.mobile-product-price').text(shades[3].price);
+    $("#mobile-products .peppers-product img").attr("src", shades[3].img);
   }
 }
